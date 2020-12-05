a5cc80e255bee7f373170a7c57b6e9743a3d7b0c
linqingzuan <linqingzuan2016@sina.com>
2020-12-05 16:12:11 +0800

    完善并优化代码。

--- api/customer_tagconfig.py
+++ api/customer_tagconfig.py
@@ -2,62 +2,80 @@ import json
 
 import requests
 
+CORPID = 'wwaa7408f7b2a969d9'
+CORSECRET = '6r6i1urQJBPURvUyVFmklyZXRN8P409Xn74n1ia6FPk'
+
 
 class TagConfig:
+    def __init__(self):
+        self.token = ""
+
     def get_token(self):
-        CORPID = 'wwaa7408f7b2a969d9'
-        CORSECRET = '6r6i1urQJBPURvUyVFmklyZXRN8P409Xn74n1ia6FPk'
         res = requests.get(
-            'https://qyapi.weixin.qq.com/cgi-bin/gettoken?',
+            'https://qyapi.weixin.qq.com/cgi-bin/gettoken',
             params={
                 'corpid': CORPID,
                 'corpsecret': CORSECRET
             }
         )
 
+        self.token = res.json()['access_token']
+
+    def clear_env(self):
+        print("######开始做清理数据啦~~~")
+        group_id_list = []
+        # 获取插入数据后的tag数据
+        res_tag_list = self.get_configlist({"tag_id": []})
+        print("#########获取原始数据", json.dumps(res_tag_list.json(), indent=2))
+        # 判断一下拿到的数据有没有问题
+        assert res_tag_list.status_code == 200
+        assert res_tag_list.json()['errcode'] == 0
+
+        # 获取返回数据中的group id
+        group_id_l = [{'group_id': group['group_id']} for group in res_tag_list.json()['tag_group']]
+        # 将group id转换成数组
+        for group_id in group_id_l:
+            group_id_list.append(group_id['group_id'])
+        print("#####group_id_list is", group_id_list)
+
+        # 如果我的数据本来就是空，那我不做任何处理，否则才会去删除
+        if len(group_id_list) == 0:
+            print("############列表为空，不做任何处理")
+            pass
+        else:
+            res = requests.post(
+                'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/del_corp_tag',
+                params={'access_token': self.token},
+                json={
+                    "group_id": group_id_list
+                }
+            )
+            print("#########处理结果", json.dumps(res.json(), indent=2))
+
+    def get_configlist(self, testcase):
+        res = requests.post(
+            'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list',
+            params={'access_token': self.token},
+            json=testcase
+        )
+
         # print(json.dumps(res.json(), indent=2))
-        return res.json()['access_token']
-
-    def get_configlist(self):
-        token = self.get_token()
-        res = requests.post(
-            'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list?',
-            params={'access_token': token},
-            json={
-                'tag_id': []
-            }
-        )
-
-        print(json.dumps(res.json(), indent=2))
         return res
 
-    def add_tag(self):
-        token = self.get_token()
+    def add_tag(self, tag_list):
         res = requests.post(
-            'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/add_corp_tag?',
-            params={'access_token': token},
-            json={
-                "group_name": "group_demo_122211",
-                "tag": [{
-                        "name": "tag_demo_122211",
-                    }
-                ]
-            }
+            'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/add_corp_tag',
+            params={'access_token': self.token},
+            json=tag_list
         )
-        print(json.dumps(res.json(), indent=2))
+        # print(json.dumps(res.json(), indent=2))
         return res
 
-    def del_tag(self):
-        token = self.get_token()
+    def del_tag(self, testcases):
         res = requests.post(
-            'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/del_corp_tag?',
-            params={'access_token': token},
-            json={
-                "tag_id": [
-                    "etqsg8CAAAw9LPAug7SzJSSZVTWIXspA"
-                ]
-            }
+            'https://qyapi.weixin.qq.com/cgi-bin/externalcontact/del_corp_tag',
+            params={'access_token': self.token},
+            json=testcases
         )
-        print(json.dumps(res.json(), indent=2))
+        # print(json.dumps(res.json(), indent=2))
         return res
-
--- testcases/test_tagconfig.py
+++ testcases/test_tagconfig.py
@@ -1,21 +1,119 @@
+import json
+
+import pytest
+import yaml
+
 from api.customer_tagconfig import TagConfig
 
 
 class TestTagconfig:
-    def test_list_tag(self):
-        res = TagConfig().get_configlist()
+
+    def setup_class(self):
+        self.tagConfig = TagConfig()
+        # 获取token
+        self.tagConfig.get_token()
+        # 数据清理
+        self.tagConfig.clear_env()
+
+    # 用yaml来作为数据驱动
+    """
+    test_list_tag:
+    case1:带tagid查询
+    case2:tagid为空查询
+    """
+
+    @pytest.mark.parametrize("testcase", yaml.safe_load(open("cases/get.yaml", encoding="utf-8")).values())
+    def test_list_tag(self, testcase):
+        res = self.tagConfig.get_configlist(testcase)
         assert res.status_code == 200
         assert res.json()['errcode'] == 0
 
-    def test_add_tag(self):
-        res = TagConfig().add_tag()
+    """
+    test_add_tag:
+    case1:只带tag.name
+    case2:tag.name长度为30,并添加到已存在的标签组
+    case3:带上所有非必须optional
+    case4:group.name长度为30
+    case5:order/tag.order为0
+    case6:order/tag.order为2^32
+    """
+
+    @pytest.mark.parametrize("tag_list", yaml.safe_load(open("cases/add_tag.yaml", encoding="utf-8")).values())
+    def test_add_tag(self, tag_list):
+        # self.test_flag = 0
+        res = self.tagConfig.add_tag(tag_list)
         assert res.status_code == 200
         assert res.json()['errcode'] == 0
 
-    def test_add_fail(self):
-        pass
+        # 获取插入数据后的tag数据
+        res_list = self.tagConfig.get_configlist({"tag_id": []})
+        assert res_list.status_code == 200
+        assert res_list.json()['errcode'] == 0
 
-    def test_del_tag(self):
-        res = TagConfig().del_tag()
+        # 获取add的tag name list
+        expect_list = [{'name': tag['name']} for tag in tag_list['tag']]
+
+        group = [group for group in res_list.json()['tag_group'] if group['group_name'] == tag_list['group_name']][0]
+        tags = [{'name': tag['name']} for tag in group['tag']]
+
+        # 断言插入的组名是否存在
+        assert group['group_name'] == tag_list['group_name']
+        # 断言插入的tag name是否存在
+        for item in expect_list:
+            assert (item in tags)
+
+    """
+    test_add_fail:
+    case1:tag.name长度>30
+    case2:group.name长度>30
+    case3:tag.name为空
+    case4:group.name为空
+    case5:order边界值<0
+    case6:order边界值>2^32
+    """
+
+    @pytest.mark.parametrize("testcases", yaml.safe_load(open("cases/add_exception.yaml", encoding="utf-8")).values())
+    def test_add_fail(self, testcases):
+        res = self.tagConfig.add_tag(testcases)
+        print("#########获取原始数据", json.dumps(res.json(), indent=2))
+        assert res.status_code == 200
+        assert res.json()['errcode'] != 0
+
+    """
+    test_del_tag:
+    case1:json只带tagid
+    case2:json只带groupid
+    case3:json带tagid和groupid
+    """
+
+    @pytest.mark.parametrize("testcases", yaml.safe_load(open("./cases/delete.yaml", encoding="utf-8")).values())
+    def test_del_tag(self, testcases):
+        res = self.tagConfig.del_tag(testcases)
         assert res.status_code == 200
         assert res.json()['errcode'] == 0
+
+        # 获取插入数据后的tag数据
+        res_tag_list = self.tagConfig.get_configlist({"tag_id": []})
+        print(json.dumps(res_tag_list.json(), indent=2))
+        assert res_tag_list.status_code == 200
+        assert res_tag_list.json()['errcode'] == 0
+
+        # 获取返回数据中的id
+        group_id_list = [{'group_id': group['group_id']} for group in res_tag_list.json()['tag_group']]
+        tag_id_list = [{'id': tag['id']} for tag_group in res_tag_list.json()['tag_group'] for tag in tag_group['tag']]
+
+        if dict(testcases).keys() == 'tag_id':
+            assert (testcases not in tag_id_list)
+        elif dict(testcases).keys() == 'group_id':
+            assert (testcases not in group_id_list)
+
+    """
+    case1:del一个不存在的tag.id
+    case2:del一个不存在的group.id
+    """
+    @pytest.mark.parametrize("testcases", yaml.safe_load(open("cases/del_exception.yaml", encoding="utf-8")).values())
+    def test_del_fail(self, testcases):
+        res = self.tagConfig.add_tag(testcases)
+        print("#########获取原始数据", json.dumps(res.json(), indent=2))
+        assert res.status_code == 200
+        assert res.json()['errcode'] != 0
